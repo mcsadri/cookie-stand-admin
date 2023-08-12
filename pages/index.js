f9494e77c512c119b5284f2e842ab2abf583ca0a
Manuch Sadri <colonquebang@hotmail.com>
2023-08-11 23:41:02 -0700

    updates index.js with form layout and styling

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,60 @@ export default function Home() {
         <title>Cookie Stand Admin</title>
       </Head>
 
-      <header class="flex items-center justify-between bg-green-500 text-black p-5">
-        <h1>Cookie Stand Admin</h1>
+      <header className="flex items-center justify-between bg-green-500 text-black text-4xl font-sans font-semibold p-5">
+        <p>Cookie Stand Admin</p>
         <p>X locations added</p>
       </header>
 
-      <main className="flex flex-col items-center py-4 space-y-8">
-        <p>Sample Test. Create Cookie Stand</p>
+      <main className="flex flex-col items-center justify-center font-sans text-black p-5">
+
+        <form className="flex flex-col items-center justify-evenly w-3/4 p-2 my-4 bg-green-300 rounded-lg">
+
+          <div className="flex items-center justify-center text-3xl p-4">
+              Create Cookie Stand
+          </div>
+
+          <div className="flex items-center flex-col w-full p-2 text-xl">
+
+            <div className="flex items-center flex-row w-full">
+              <label className="flex p-2">Location</label>
+              <input type="text" name="location" id="id_location" className="flex-auto pl-1 "/>
+            </div>
+
+            <div className="flex flex-row p-4 items-center justify-evenly text-center">
+
+              <div className="flex flex-col p-2 ">
+                <label>Minimum Customers per Hour</label>
+                <input type="number" min="0" name="min_cust" id="id_min_cust"></input>
+              </div>
+
+              <div className="flex flex-col items-center p-2">
+                <label>Maximum Customers per Hour</label>
+                <input type="number" min="1" name="max_cust" id="id_max_cust"></input>
+              </div>
+
+              <div className="flex flex-col items-center p-2">
+                <label>Average Cookies per Sale</label>
+                <input type="number" step="0.1" min="0.0" name="hourly" id="id_hourly"></input>
+              </div>
+
+              <div>
+                <button className="px-20 py-8 bg-green-500" name="" id="id_">Create</button>
+              </div>   
+              
+            </div>
+
+          </div>
+
+        </form>
+
+        <div className="text-xl p-10">
+          <p>Report Table Coming Soon...</p>
+        </div>
+
       </main>
 
-      <footer class="flex items-center justify-between bg-green-500 text-black p-5">
+      <footer className="flex items-center justify-between bg-green-500 text-black p-5">
         <p>&copy;2023</p>
       </footer>
 
